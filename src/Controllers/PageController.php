5c2e872cfb89efdbf6ef85d885b2fee5144afc64
Nicolas-peter68 <71389477+Nicolas-peter68@users.noreply.github.com>
2021-01-04 16:57:24 +0100

    first commit

---
+++ src/Controllers/PageController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\MovieModel;
+
+class PageController extends GeneralController
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        //$movieModel = new MovieModel();
+        //$movies = $movieModel->getAllMovies();
+
+        //$template = $this->twig->load('Page/index.html.twig');
+        //echo $template->render(["movies" => $movies]);
+        echo "index";
+    }
+
+    public function error404()
+    {
+        /*
+        $template = $this->twig->load('Page/error404.html.twig');
+        echo $template->render();*/
+        echo "404";
+    }
+}
