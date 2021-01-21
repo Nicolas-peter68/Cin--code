dc10a6adb9e46669045c8f29e85edc1a2e217d24
Nicolas-peter68 <71389477+Nicolas-peter68@users.noreply.github.com>
2021-01-21 11:00:27 +0100

    filtrage des acteurs

--- src/Models/ActorsModel.php
+++ src/Models/ActorsModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 class ActorsModel extends GeneralModel{
     public function getActorsById($id)
     {
-        $sql = "SELECT * FROM artistes WHERE artistes.id=?";
+        $sql = "SELECT * FROM artistes, artiste_role WHERE artistes.id=? AND id_role=1 AND artistes.id=artiste_role.id_artiste";
         $req = $this->pdo->prepare($sql);
         $req->execute([$id]);
         return $req->fetch();
